e68a2a115fdf743f782698aa018476b2560e300a
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-08-12 19:29:37 +0100

    added notes to augmented file

---
+++ augmented-math-operations.js
@@ -0,0 +1,41 @@
+// COMPOUND ASSIGNMENT WITH AUGMENTED ADDITION
+
+    // IT IS COMMON TO ADD NUMBERS TO A VARIABLE, INITIALLY AND DOWN THE LINE:
+
+    var a = 3;
+    var b = 17;
+    var c = 12;
+
+    a = a + 12;
+    b = 9 + b;
+    c = c + 7;
+
+    console.log(a, b, c);       // OUTPUTS 15, 26, 19
+
+    // BECAUSE THIS IS SUCH A COMMON PATTERN, THERE'S A SHORTCUT TO ACHIEVE THE SAME THING USING THE += OPERATOR:
+
+    a += 12;
+    b += 9;
+    c += 7;
+
+    console.log(a, b, c);       // OUTPUTS 15, 26, 19 (NEGATING THE ABOVE)
+
+    // THIS WAY, WE ADD THE VALUE TO THE VARIABLE AND ASSIGN THE ANSWER TO THAT VARIABLE SIMULTANEOUSLY
+
+// COMPOUND ASSIGNMENT WITH AUGMENTED SUBTRACTION
+
+    // SIMILAR TO ABOVE, THE -= OPERATOR SUBTRACTS THE VALUE FROM THE VARIABLE AND ASSIGNS THE ANSWER TO IT
+
+    var x = 11;
+    var y = 9;
+    var z = 3;
+
+    x -= 6;
+    y -= 15;
+    z -= 1;
+
+    console.log(x, y, z);   // OUTPUTS 5, -6, 2
+
+// COMPOUND ASSIGNMENT WITH AUGMENTED MULTIPLICATION
+
+    
