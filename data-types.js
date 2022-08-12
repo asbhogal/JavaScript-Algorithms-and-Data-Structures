afa845ee63c87f1674a98d839cfe38c73ddb984e
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-08-12 13:14:09 +0100

    added comments

--- data-types.js
+++ data-types.js
@@ -1,11 +1,14 @@
+// data is anything meaningful to a computer. JS provides 7 different types that can be used
+
 /* Data Types:
 
     -   undefined e.g. a variable not set to anything yet
     -   null
     -   boolean
-    -   string
+    -   string*
     -   symbol (immutable primitive value which is unique)
-    -   number
+    -   number*
     -   object (can store many key value pairs)
 
+    *most commonly used
 */
