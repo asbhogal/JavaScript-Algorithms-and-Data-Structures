19901a24a51bda2a232b121244bfd23d5d3cb436
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-15 22:57:21 +0100

    added 'catch-misspelled...' notes to 'Debugging' dir

---
+++ Debugging/catch-misspelled-variable-and-function-names.js
@@ -0,0 +1,11 @@
+// Fix the two spelling errors in the code so the netWorkingCapital calculation works.
+
+/*let receivables = 10;
+let payables = 8;
+let netWorkingCapital = recievables - payable;
+console.log(`Net working capital is: ${netWorkingCapital}`);*/
+
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
