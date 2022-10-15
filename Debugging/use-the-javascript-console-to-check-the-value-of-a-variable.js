b654cee52985be8e5b3e5b1c85a42c82ffc75a05
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-15 22:50:06 +0100

    add. notes, created 'ES6' and 'Debugging' dirs and organised content

---
+++ Debugging/use-the-javascript-console-to-check-the-value-of-a-variable.js
@@ -0,0 +1,9 @@
+let a = 5;
+let b = 1;
+a++;
+// Only change code below this line
+
+// Use the console.log() method to print the value of the variable a where noted in the code.
+
+let sumAB = a + b;
+console.log(a);
---
+++ Debugging/use-typeof-to-check-the-type-of-a-variable.js
@@ -0,0 +1,11 @@
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Only change code below this line
+
+/*JavaScript recognizes seven primitive (immutable) data types: Boolean, Null, Undefined, Number, String, Symbol (new with ES6), and BigInt (new with ES2020), and one type for mutable items: Object. Note that in JavaScript, arrays are technically a type of object.
+
+Add two console.log() statements to check the typeof each of the two variables seven and three in the code.*/
+
+console.log(typeof seven);
+console.log(typeof three);
