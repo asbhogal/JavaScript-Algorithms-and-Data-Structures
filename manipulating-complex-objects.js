04d6a2d86cb43be7a5e46ab5012aefb34660061a
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-15 21:59:12 +0100

    uploaded add. notes from course activities & exercises

---
+++ manipulating-complex-objects.js
@@ -0,0 +1,22 @@
+const myMusic = [
+  {
+    "artist"        : "Billy Joel",
+    "title"         : "Piano Man",
+    "release_year"  : 1973,
+    "formats"       : [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold"          :   true
+  },
+  {
+    "artist"        :   "Miles Davis",
+    "title"         :   "Kind of Blue",
+    "release_year"  :   1959,
+    "formats"       :   [
+      "CD",
+      "LP"
+    ]
+  }
+];
