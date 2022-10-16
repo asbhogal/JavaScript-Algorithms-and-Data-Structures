eeeb647e74367993a7079c586fbe32c0b4a29c84
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-16 14:02:36 +0100

    added 'Basic Data Structures' dir and resp. course notes

---
+++ Basic Data Structures/access-an-arrays-contents-using-bracket-notation.js
@@ -0,0 +1,9 @@
+// In order to complete this challenge, set the 2nd position (index 1) of myArray to anything you want, besides the letter b.
+
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+
+myArray[1] = "f";
+
+// Only change code above this line
+console.log(myArray);
---
+++ Basic Data Structures/use-an-array-to-store-a-collection-of-data.js
@@ -0,0 +1,11 @@
+// let yourArray; // Change this line
+
+// We have defined a variable called yourArray. Complete the statement by assigning an array of at least 5 elements in length to the yourArray variable. Your array should contain at least one string, one number, and one boolean.
+
+let yourArray = [
+  'one',
+  5,
+  true,
+  undefined,
+  null
+]
