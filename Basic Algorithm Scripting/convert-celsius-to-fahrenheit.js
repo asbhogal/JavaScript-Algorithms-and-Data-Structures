0399b0799e835c791aef518b4600496ba87313d2
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-16 22:41:23 +0100

    added 'Basic Algorithm Scripting' and 'Regular Expression' dirs and subs. course notes

---
+++ Basic Algorithm Scripting/convert-celsius-to-fahrenheit.js
@@ -0,0 +1,9 @@
+// You are given a variable celsius representing a temperature in Celsius. Use the variable fahrenheit already defined and assign it the Fahrenheit temperature equivalent to the given Celsius temperature. Use the formula mentioned above to help convert the Celsius temperature to Fahrenheit.
+
+function convertCtoF(celsius) {
+  let fahrenheit;
+  fahrenheit = ((celsius * (9 / 5)) + 32);
+  return fahrenheit;
+}
+
+convertCtoF(30);
