63d2eb21069838e1b810ea5b5284cad032d48220
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-21 20:34:27 +0100

    added further course notes to ES6 dir

---
+++ ES6/handle-a-rejected-promise-with-catch.js
@@ -0,0 +1,20 @@
+// Add the catch method to your promise. Use error as the parameter of its callback function and log error to the console.
+
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to false to represent an unsuccessful response from a server
+  let responseFromServer = false;
+    
+  if(responseFromServer) {          // ie. if responseFromServer = true
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+});
+
+makeServerRequest.then(result => {
+  console.log(result);
+});
+
+makeServerRequest.catch(error => {
+  console.log(error);
+})
