e43eeca2656110442be29ab991508b9d718d3251
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-21 21:54:51 +0100

    added course notes to 'Basic Data Structures' dir and amended notes

--- Basic Data Structures/add-key-value-pairs-to-javascript-objects.js
+++ Basic Data Structures/add-key-value-pairs-to-javascript-objects.js
@@ -8,11 +8,11 @@ let foods = {
 
 // Only change code below this line
 
-const strawberries = 'strawberries';
+const strawberries = 'strawberries';  // VARIABLE DECLARED THEN USED IN LINE 15
 
-foods.bananas = 13;               // DOT NOTATION METHOD
-foods['grapes'] = 35;                // BRACKET NOTATION METHOD
-foods[strawberries] = 27;         // VARIABLE NOTATION METHOD
+foods.bananas = 13;                   // DOT NOTATION METHOD
+foods['grapes'] = 35;                 // BRACKET NOTATION METHOD
+foods[strawberries] = 27;             // VARIABLE NOTATION METHOD
 
 // Only change code above this line
 
