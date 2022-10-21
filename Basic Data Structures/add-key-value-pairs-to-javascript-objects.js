92dd8651f5b6e40657489c4e8e8c92ad9678498d
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-21 21:00:45 +0100

    added course content to 'Basic Data Structures' dir

---
+++ Basic Data Structures/add-key-value-pairs-to-javascript-objects.js
@@ -0,0 +1,19 @@
+// A foods object has been created with three entries. Using the syntax of your choice, add three more entries to it: bananas with a value of 13, grapes with a value of 35, and strawberries with a value of 27.
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+};
+
+// Only change code below this line
+
+const strawberries = 'strawberries';
+
+foods.bananas = 13;               // DOT NOTATION METHOD
+foods['grapes'] = 35;                // BRACKET NOTATION METHOD
+foods[strawberries] = 27;         // VARIABLE NOTATION METHOD
+
+// Only change code above this line
+
+console.log(foods);
