2f1cade921303ab048603f3e5aae32cce19ec1ad
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-26 16:03:25 +0100

    added course notes to 'Basic Data Structures' dir

---
+++ Basic Data Structures/check-for-the-presence-of-an-element-with-indexof.js
@@ -0,0 +1,13 @@
+// indexOf() can be incredibly useful for quickly checking for the presence of an element on an array. We have defined a function, quickCheck, that takes an array and an element as arguments. Modify the function using indexOf() so that it returns true if the passed element exists on the array, and false if it does not.
+
+function quickCheck(arr, elem) {
+  // Only change code below this line
+
+  let crossCheck = arr.indexOf(elem);
+
+  return crossCheck == -1 ? false : true;
+  
+  // Only change code above this line
+}
+
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
---
+++ Basic Data Structures/copy-array-items-using-slice.js
@@ -0,0 +1,10 @@
+// We have defined a function, forecast, that takes an array as an argument. Modify the function using slice() to extract information from the argument array and return a new array that contains the string elements warm and sunny.
+
+function forecast(arr) {
+  // Only change code below this line
+  let newArray = arr.slice(2, 4);  // both index parameters start from 'cold'
+  return newArray;
+}
+
+// Only change code above this line
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
