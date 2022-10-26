9be1b8cb9c4ec5ad8871543db411208a6cd1e30b
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-26 11:20:50 +0100

    added course notes to 'Regular Expressions' dir

---
+++ Regular Expressions/match-characters-that-occur-one-or-more-times.js
@@ -0,0 +1,9 @@
+// You want to find matches when the letter s occurs one or more times in Mississippi. Write a regex that uses the + sign.
+
+// let difficultSpelling = "Mississippi";
+// let myRegex = /change/; // Change this line
+// let result = difficultSpelling.match(myRegex);
+
+let difficultSpelling = "Mississippi";
+let myRegex = /s+/g;     // i optional dep. on result and string
+let result = difficultSpelling.match(myRegex);
