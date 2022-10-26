b32b0b8537a0703b457bebc4314ca23aac024bbd
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-26 20:08:56 +0100

    added course notes to 'Basic Data Structures'

---
+++ Basic Data Structures/access-property-names-with-bracket-notation.js
@@ -0,0 +1,18 @@
+//We've defined a function, checkInventory, which receives a scanned item as an argument. Return the current value of the scannedItem key in the foods object. You can assume that only valid keys will be provided as an argument to checkInventory.
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+function checkInventory(scannedItem) {
+  // Only change code below this line
+  return foods[scannedItem]; // Passes the value to the object property and return the value via bracket notation
+  // Only change code above this line
+}
+
+console.log(checkInventory("apples"));
