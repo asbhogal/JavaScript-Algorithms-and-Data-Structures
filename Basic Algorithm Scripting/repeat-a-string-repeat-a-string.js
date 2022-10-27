6b3282df294a0be5d926681887ed693aa9c8ec4d
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-27 13:40:17 +0100

    updated course notes

--- Basic Algorithm Scripting/repeat-a-string-repeat-a-string.js
+++ Basic Algorithm Scripting/repeat-a-string-repeat-a-string.js
@@ -21,3 +21,5 @@ function repeatStringNumTimes(str, num) {
 console.log(repeatStringNumTimes("abc", 2))
 
 // Math.sign will check if a value passed to it is negative or not. If it is, it will return -1. This is the first 'elimination' which can be considered, by reasonable means, to be appropriate to parse first, before any other code on the arguments passed.
+
+// newString MUST BE DECLARED INSIDE THE else, OTHERWISE IF DECLARED OUTSIDE THE LOOP, THE num, EVEN IF IT IS NEGATIVE, WILL BE PARSED, RESULTING IN AN invalid MESSAGE. THIS MUST BE FILTERED OUT, AND THE VARIABLE THEN DECLARED LOCALLY AFTERWARDS
