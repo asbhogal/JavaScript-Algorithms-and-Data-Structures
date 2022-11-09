d694285d1b1f2ae7bb7193d895f5037cacaaf717
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-11-09 19:15:43 +0000

    added course notes to ES6

--- ES6/write-arrow-functions-with-parameters.js
+++ ES6/write-arrow-functions-with-parameters.js
@@ -1,3 +1,7 @@
+// Just like a regular function, you can pass arguments into an arrow function.
+
+// Rewrite the myConcat function which appends contents of arr2 to arr1 so that the function uses arrow function syntax.
+
 const myConcat = (arr1, arr2) => arr1.concat(arr2);
 
 console.log(myConcat([1, 2], [3, 4, 5]));
