f59cde57bb02123aeb18b71b88e8be593f014e9c
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-11-12 22:20:34 +0000

    added course notes for OOP dir

---
+++ Object Oriented Programming/understand-the-prototype-chain.js
@@ -0,0 +1,12 @@
+// All objects in JavaScript (with a few exceptions) have a prototype. Also, an object’s prototype itself is an object. Because a prototype is an object, a prototype can have its own prototype. Modify the code to show the correct prototype chain.
+
+function Dog(name) {
+  this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+Dog.prototype.isPrototypeOf(beagle);  // yields true
+
+// Fix the code below so that it evaluates to true
+Object.prototype.isPrototypeOf(Dog.prototype);
