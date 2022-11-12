4242ae3fa350dd99418193c4891e9eb1f7e45a0a
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-11-12 22:55:31 +0000

    added course notes to OOP dir

---
+++ Object Oriented Programming/inherit-behaviors-from-a-supertype.js
@@ -0,0 +1,18 @@
+//Use Object.create to make two instances of Animal named duck and beagle.
+
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+// Only change code below this line
+
+let duck = Object.create(Animal.prototype); // Change this line
+let beagle = Object.create(Animal.prototype); // Change this line
+
+console.log(duck instanceof Animal);    // RETURNS true
+console.log(beagle instanceof Animal);  // RETURNS true
---
+++ Object Oriented Programming/use-inheritance-so-you-dont-repeat-yourself.js
@@ -0,0 +1,32 @@
+// There's a principle in programming called Don't Repeat Yourself (DRY). The reason repeated code is a problem is because any change requires fixing code in multiple places. This usually means more work for programmers and more room for errors.
+
+// The eat method is repeated in both Cat and Bear. Edit the code in the spirit of DRY by moving the eat method to the Animal supertype.
+
+function Cat(name) {
+  this.name = name;
+}
+
+Cat.prototype = {
+  constructor: Cat,
+};
+
+/*************************** */
+
+function Bear(name) {
+  this.name = name;
+}
+
+Bear.prototype = {
+  constructor: Bear,
+};
+
+/*************************** */
+
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+    eat: function() {
+    console.log("nom nom nom");
+  }
+};
