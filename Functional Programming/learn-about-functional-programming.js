54ff7deb0545a3435af58360363076ce62c42f3c
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-05-14 13:02:31 +0100

    updated course notes

--- Functional Programming/learn-about-functional-programming.js
+++ Functional Programming/learn-about-functional-programming.js
@@ -1,9 +1,11 @@
-const prepareTea = () => 'greenTea';
+// In the code editor, the prepareTea and getTea functions are already defined for you. Call the getTea function to get 40 cups of tea for the team, and store them in the tea4TeamFCC variable.
+
+const prepareTea = () => "greenTea";
 
 const getTea = (numOfCups) => {
   const teaCups = [];
 
-  for(let cups = 1; cups <= numOfCups; cups += 1) {
+  for (let cups = 1; cups <= numOfCups; cups += 1) {
     const teaCup = prepareTea();
     teaCups.push(teaCup);
   }
