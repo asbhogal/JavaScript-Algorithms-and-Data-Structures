ab07e56d3e3ee92159f42fe5b5403672cf1f452e
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-05-14 13:43:39 +0100

    added course notes

---
+++ Functional Programming/avoid-mutations-and-side-effects-using-functional-programming.js
@@ -0,0 +1,8 @@
+// Fill in the code for the function incrementer so it returns the value of the global variable fixedValue increased by one.
+
+let fixedValue = 4;
+
+function incrementer() {
+  let newValue = fixedValue + 1
+  return newValue
+}
--- Functional Programming/understand-functional-programming-terminology.js
+++ Functional Programming/understand-functional-programming-terminology.js
@@ -1,3 +1,7 @@
+/* Prepare 27 cups of green tea and 13 cups of black tea and store them in tea4GreenTeamFCC and tea4BlackTeamFCC variables, respectively. Note that the getTea function has been modified so it now takes a function as the first argument.
+
+Note: The data (the number of cups of tea) is supplied as the last argument. We'll discuss this more in later lessons.
+ */
 // Function that returns a string representing a cup of green tea
 const prepareGreenTea = () => 'greenTea';
 
